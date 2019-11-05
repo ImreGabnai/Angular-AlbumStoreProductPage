842632bbb1db891cb6ec1e57a2aba69cd4a7c0ec
ImreGabnai <32438501+ImreGabnai@users.noreply.github.com>
2019-11-05 09:52:53 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { ProductPageModule } from './product-page/product-page.module';
 import { CommonModule } from '@angular/common';
+=======
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+>>>>>>> parent of 50c1ad7... Working
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     ProductPageModule,
+=======
+    ProductPageComponent
+>>>>>>> parent of 50c1ad7... Working
   ],
   imports: [
     BrowserModule,
     FormsModule,
+<<<<<<< HEAD
     CommonModule,
     ProductPageModule,
+=======
+    HttpModule
+>>>>>>> parent of 50c1ad7... Working
   ],
   providers: [],
   bootstrap: [AppComponent],
